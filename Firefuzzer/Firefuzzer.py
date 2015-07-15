50c6c2da10a0673b88741320497dae12aa39696a
FakerKimg <fakefree777+gh000@gmail.com>
2015-07-15 12:34:34 +0800

    a little change

--- Firefuzzer/Firefuzzer.py
+++ Firefuzzer/Firefuzzer.py
@@ -12,6 +12,21 @@ class Firefuzzer():
         self.detail = detail
 
     def run(self):
+        print ""
+        print "\t8888888888 8888888 8888888b.  8888888888 8888888888 888     888 8888888888P 8888888888P 8888888888 8888888b."
+        print "\t888          888   888   Y88b 888        888        888     888       d88P        d88P  888        888   Y88b "
+        print "\t888          888   888    888 888        888        888     888      d88P        d88P   888        888    888 "
+        print "\t8888888      888   888   d88P 8888888    8888888    888     888     d88P        d88P    8888888    888   d88P "
+        print "\t888          888   8888888P\"  888        888        888     888    d88P        d88P     888        8888888P\" "
+        print "\t888          888   888 T88b   888        888        888     888   d88P        d88P      888        888 T88b "
+        print "\t888          888   888  T88b  888        888        Y88b. .d88P  d88P        d88P       888        888  T88b"
+        print "\t888        8888888 888   T88b 8888888888 888         \"Y88888P\"  d8888888888 d8888888888 8888888888 888   T88b "
+        print ""
+
+        print "########################################################################################################################"
+        print "Targeted URL : " + self.url
+        print "########################################################################################################################"
+
         if mode == "buffer":
             overflow = BufferOverflow.BufferOverflow(self,url, self,detail)
             overflow.parseInput()
@@ -19,6 +34,10 @@ class Firefuzzer():
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
+        print "Incorrect number of parameters"
+        print "Wyntax is \n\tpython Firefuzzer <url> <buffer> <detail(OPTIONAL)>"
+
     args = sys.argv
     detail = len(args) >= 4 and args[3] == "detail"
 
