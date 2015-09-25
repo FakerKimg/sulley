d213513a284e7b1f93d1562a1760d10302bc97a1
FakerKimg <fakefree777+gh000@gmail.com>
2015-09-25 21:06:13 +0800

    mimic the hyper example, but there might be some problem on frame format......

---
+++ http2_fuzz.py
@@ -0,0 +1,38 @@
+from sulley import *
+from utils.html_parser import *
+from requests import base_http2_frame
+
+def clean_receive_buffer(sock):
+    try:
+        sock.recv(4096)
+    except:
+        pass
+
+def update_stream_id(session, node, edge, sock):
+    try:
+        sock.recv(4096)
+    except:
+        pass
+    stream_id = 1
+    #s_update("header stream id", stream_id)
+    #s_update("data stream id", stream_id)
+
+
+sess = sessions.session(sleep_time = 0.0001)
+target = sessions.target("104.131.161.90", 80)
+sess.add_target(target)
+
+
+headers = {"method": "GET", "scheme": "http", "authority": "http2bin", "path": "/ip", "asdf": "asdf"}
+base_http2_frame.set_header_frame(headers)
+
+sess.pre_send = clean_receive_buffer
+sess.connect(sess.root, s_get("HTTP/2 Magic"), callback=None)
+sess.connect(s_get("HTTP/2 Magic"), s_get("HTTP/2 Settings disable push"), callback=None)
+sess.connect(s_get("HTTP/2 Settings disable push"), s_get("HTTP/2 Settings ACK"), callback=update_stream_id)
+sess.connect(s_get("HTTP/2 Settings ACK"), s_get("HTTP/2 Headers"), callback=None)
+sess.connect(s_get("HTTP/2 Headers"), s_get("HTTP/2 Datas"), callback=None)
+sess.post_send = clean_receive_buffer
+
+sess.fuzz()
+
---
+++ requests/base_http2_frame.py
@@ -0,0 +1,66 @@
+from sulley import *
+from hyper.packages.hpack.hpack import Encoder, Decoder
+
+########################################################################################################################
+# Magic frame(Preamble packet) in HTTP/2 protocol
+########################################################################################################################
+s_initialize("HTTP/2 Magic")
+s_static(b"PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n")
+
+########################################################################################################################
+# Settings packet for disable push  in HTTP/2 protocol
+########################################################################################################################
+s_initialize("HTTP/2 Settings disable push")
+s_bit_field(6, 24, fuzzable=False) # length(bytes number) of payload in frame
+s_bit_field(4, 8, fuzzable=False)  # type of frame
+s_bit_field(0, 8, fuzzable=False)  # flags of frame
+s_bit_field(0, 32, fuzzable=False) # stream ID
+# payload
+s_bit_field(2, 16, fuzzable=False) # settings enable_push
+s_bit_field(0, 32, fuzzable=False) # disable
+
+########################################################################################################################
+# Settings packet for ACK in HTTP/2 protocol
+########################################################################################################################
+s_initialize("HTTP/2 Settings ACK")
+s_bit_field(0, 24, fuzzable=False) # length(bytes number) of payload in frame
+s_bit_field(4, 8, fuzzable=False)  # type of frame
+s_bit_field(1, 8, fuzzable=False)  # flags of frame
+s_bit_field(0, 32, fuzzable=False) # stream ID
+
+########################################################################################################################
+# Headers packet in HTTP/2 protocol
+########################################################################################################################
+def set_header_frame(headers):
+    s_initialize("HTTP/2 Headers")
+    s_size("headers payload", length=3, format="binary") # length(bytes number) of payload in frame
+    s_bit_field(1, 8, fuzzable=False)  # type of frame
+    s_bit_field(4, 8, fuzzable=False)  # flags of frame, End Headers
+    s_bit_field(1, 32, fuzzable=False, name="header stream id") # stream ID
+    # payload (Header block fragment)
+    if s_block_start("headers payload"):
+        e = Encoder()
+        s_string(e.encode(headers), encoding="binary", fuzzable=False)
+        """
+        for key, value in headers:
+            s_bit_field(1+len(key), 32, fuzzable=False)
+            s_string(":"+key, encoding="binary", fuzzable=False, max_length=0)
+            s_bit_field(len(value), 32, fuzzable=True)
+            s_string(value, encoding="binary", fuzzable=False, max_length=0)
+        """
+    s_block_end()
+
+########################################################################################################################
+# Datas packet in HTTP/2 protocol
+########################################################################################################################
+s_initialize("HTTP/2 Datas")
+s_size("datas payload", length=3, format="binary") # length(bytes number) of payload in frame
+s_bit_field(0, 8, fuzzable=False)  # type of frame
+s_bit_field(1, 8, fuzzable=False)  # flags of frame, End Stream
+s_bit_field(1, 32, fuzzable=False, name="data stream id") # stream ID
+# payload (Header block fragment)
+if s_block_start("datas payload"):
+    s_string("", encoding="binary", fuzzable=True)
+s_block_end()
+
+
