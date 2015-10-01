07b863f586ccb39338370652c590ffd5eedd17d4
FakerKimg <fakefree777+gh000@gmail.com>
2015-10-01 21:22:57 +0800

    mimic successfully

--- http2_fuzz.py
+++ http2_fuzz.py
@@ -22,8 +22,8 @@ sess = sessions.session(sleep_time = 0.0001)
 target = sessions.target("104.131.161.90", 80)
 sess.add_target(target)
 
-
-headers = {"method": "GET", "scheme": "http", "authority": "http2bin", "path": "/ip", "asdf": "asdf"}
+headers = {":method": "GET", ":scheme": "http", ":authority": "http2bin.org", ":path": "/ip", "asdf": "asdf"}
+#headers = [(":method", "GET"), (":scheme", "http"), (":authority", "http2bin.org"), (":path", "/ip"), ("asdf", "asdf")]
 base_http2_frame.set_header_frame(headers)
 
 sess.pre_send = clean_receive_buffer
--- requests/base_http2_frame.py
+++ requests/base_http2_frame.py
@@ -11,19 +11,19 @@ s_static(b"PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n")
 # Settings packet for disable push  in HTTP/2 protocol
 ########################################################################################################################
 s_initialize("HTTP/2 Settings disable push")
-s_bit_field(6, 24, fuzzable=False) # length(bytes number) of payload in frame
+s_bit_field(6, 24, endian=">", fuzzable=False) # length(bytes number) of payload in frame
 s_bit_field(4, 8, fuzzable=False)  # type of frame
 s_bit_field(0, 8, fuzzable=False)  # flags of frame
-s_bit_field(0, 32, fuzzable=False) # stream ID
+s_bit_field(0, 32, endian=">", fuzzable=False) # stream ID
 # payload
-s_bit_field(2, 16, fuzzable=False) # settings enable_push
-s_bit_field(0, 32, fuzzable=False) # disable
+s_bit_field(2, 16, endian=">", fuzzable=False) # settings enable_push
+s_bit_field(0, 32, endian=">", fuzzable=False) # disable
 
 ########################################################################################################################
 # Settings packet for ACK in HTTP/2 protocol
 ########################################################################################################################
 s_initialize("HTTP/2 Settings ACK")
-s_bit_field(0, 24, fuzzable=False) # length(bytes number) of payload in frame
+s_bit_field(0, 24, endian=">", fuzzable=False) # length(bytes number) of payload in frame
 s_bit_field(4, 8, fuzzable=False)  # type of frame
 s_bit_field(1, 8, fuzzable=False)  # flags of frame
 s_bit_field(0, 32, fuzzable=False) # stream ID
@@ -33,14 +33,17 @@ s_bit_field(0, 32, fuzzable=False) # stream ID
 ########################################################################################################################
 def set_header_frame(headers):
     s_initialize("HTTP/2 Headers")
-    s_size("headers payload", length=3, format="binary") # length(bytes number) of payload in frame
+    s_size("headers payload", endian=">", length=3, format="binary") # length(bytes number) of payload in frame
     s_bit_field(1, 8, fuzzable=False)  # type of frame
     s_bit_field(4, 8, fuzzable=False)  # flags of frame, End Headers
-    s_bit_field(1, 32, fuzzable=False, name="header stream id") # stream ID
+    s_bit_field(1, 32, endian=">", fuzzable=False, name="header stream id") # stream ID
     # payload (Header block fragment)
     if s_block_start("headers payload"):
         e = Encoder()
-        s_string(e.encode(headers), encoding="binary", fuzzable=False)
+        header_pairs = []
+        for name, value in headers.iteritems():
+            header_pairs.append((name.lower(), value))
+        s_string(e.encode(header_pairs), encoding="binary", fuzzable=False)
         """
         for key, value in headers:
             s_bit_field(1+len(key), 32, fuzzable=False)
@@ -57,10 +60,9 @@ s_initialize("HTTP/2 Datas")
 s_size("datas payload", length=3, format="binary") # length(bytes number) of payload in frame
 s_bit_field(0, 8, fuzzable=False)  # type of frame
 s_bit_field(1, 8, fuzzable=False)  # flags of frame, End Stream
-s_bit_field(1, 32, fuzzable=False, name="data stream id") # stream ID
+s_bit_field(1, 32, endian=">", fuzzable=False, name="data stream id") # stream ID
 # payload (Header block fragment)
 if s_block_start("datas payload"):
     s_string("", encoding="binary", fuzzable=True)
 s_block_end()
-
 
