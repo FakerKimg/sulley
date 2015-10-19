b9ef6a6c2bcbba721abc9a5bed5cc961e0f9940f
FakerKimg <fakefree777+gh000@gmail.com>
2015-10-19 19:33:37 +0800

    add new regular requests, and fix syntax bugs

--- regular_data_generator.py
+++ regular_data_generator.py
@@ -1,7 +1,8 @@
 from data_generator.generator import generate_regular_data
 
-# telephone
 generate_regular_data("Tel")
 generate_regular_data("Text")
 generate_regular_data("Email")
+generate_regular_data("Url")
+generate_regular_data("Time")
 
