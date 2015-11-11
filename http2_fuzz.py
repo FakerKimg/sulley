c0aaf6ce578269b0cd7e89e349f9b1cf8d68485f
FakerKimg <fakefree777+gh000@gmail.com>
2015-11-11 21:36:12 +0800

    remove redundence, and solve the problems about s_group(?????)

--- http2_fuzz.py
+++ http2_fuzz.py
@@ -1,23 +1,6 @@
 from sulley import *
 from requests import base_http2_frame
 
-def clean_receive_buffer(sock):
-    try:
-        #sock.recv(4096)
-        pass
-    except:
-        pass
-
-def update_stream_id(session, node, edge, sock):
-    try:
-        #session.last_recv = sock.recv(4096)
-        pass
-    except:
-        pass
-    stream_id = 1
-    #s_update("header stream id", stream_id)
-    #s_update("data stream id", stream_id)
-
 def check_ack_settings_frame(session, node, edge, sock):
     recv = session.last_recv
     if recv and recv[3] == '\x04' and recv[4] == '\x00':
@@ -31,22 +14,13 @@ target = sessions.target("192.168.144.104", 8080)
 #target = sessions.target("104.131.161.90", 80)
 sess.add_target(target)
 
-headers = {":method": ["GET", "POST"], ":scheme": ["http"], ":authority": "192.168.144.104", ":path": "/"}
+headers = {":method": ["GET", "POST", "PUT", "DELETE"], ":scheme": ["http", "ftp"], ":authority": "192.168.144.104", ":path": "/"}
 #headers = {":method": "GET", ":scheme": "http", ":authority": "http2bin.org", ":path": "/ip"}
-base_http2_frame.set_header_frame(headers)
+base_http2_frame.set_header_data_frame(headers, "HTTP/2 Headers and Datas")
 
-sess.pre_send = clean_receive_buffer
 sess.connect(sess.root, s_get("HTTP/2 Magic"), callback=None)
 sess.connect(s_get("HTTP/2 Magic"), s_get("HTTP/2 Settings disable push"), callback=None)
-
-
-
-sess.connect(s_get("HTTP/2 Settings disable push"), s_get("HTTP/2 Headers"), callback=check_ack_settings_frame)
-#sess.connect(s_get("HTTP/2 Settings disable push"), s_get("HTTP/2 Headers"), callback=check_ack_settings_frame)
-#sess.connect(s_get("HTTP/2 Settings disable push"), s_get("HTTP/2 Settings ACK"), callback=check_ack_settings_frame)
-#sess.connect(s_get("HTTP/2 Settings ACK"), s_get("HTTP/2 Headers"), callback=check_ack_settings_frame)
-#sess.connect(s_get("HTTP/2 Headers"), s_get("HTTP/2 Datas"), callback=None)
-sess.post_send = clean_receive_buffer
+sess.connect(s_get("HTTP/2 Settings disable push"), s_get("HTTP/2 Headers and Datas"), callback=check_ack_settings_frame)
 
 sess.fuzz()
 
--- requests/base_http2_frame.py
+++ requests/base_http2_frame.py
@@ -3,6 +3,7 @@ from hyper.packages.hpack.hpack import Encoder, Decoder
 import json
 
 def json_hpack_encoder(json_headers):
+    print json_headers
     e = Encoder()
     headers = json.loads(json_headers)
     header_pairs = []
@@ -40,19 +41,11 @@ s_bit_field(2, 16, endian=">", fuzzable=False) # settings enable_push
 s_bit_field(0, 32, endian=">", fuzzable=False) # disable
 
 ########################################################################################################################
-# Settings packet for ACK in HTTP/2 protocol
-########################################################################################################################
-s_initialize("HTTP/2 Settings ACK")
-s_bit_field(0, 24, endian=">", fuzzable=False) # length(bytes number) of payload in frame
-s_bit_field(4, 8, fuzzable=False)  # type of frame
-s_bit_field(1, 8, fuzzable=False)  # flags of frame
-s_bit_field(0, 32, fuzzable=False) # stream ID
-
-########################################################################################################################
 # Headers packet in HTTP/2 protocol
 ########################################################################################################################
-def set_header_frame(headers):
-    s_initialize("HTTP/2 Headers")
+def set_header_data_frame(headers, header_name):
+    # Header
+    s_initialize(header_name)
     s_size("headers payload", endian=">", length=3, format="binary", fuzzable=False) # length(bytes number) of payload in frame
     s_bit_field(1, 8, fuzzable=False)  # type of frame
     s_bit_field(4, 8, fuzzable=False)  # flags of frame, End Headers
@@ -63,17 +56,20 @@ def set_header_frame(headers):
         s_static("{")
 
         count = 0
+        gc = 0
         for name, value in headers.iteritems():
             # name
             s_static("\"" + name + "\": ")
 
             # value
             s_static("\"")
+
             if isinstance(value, list):
                 s_group(name+"_group", values=value)
+                gc = gc +1
                 if s_block_start(name+"_block", group=name+"_group"):
                     s_static("")
-                s_block_end()
+                #s_block_end()
             else:
                s_static(value)
             
@@ -82,6 +78,10 @@ def set_header_frame(headers):
             if count + 1 != len(headers):
                 s_static(", ")
             count = count + 1
+
+        # for group.................................
+        for i in range(0, gc):
+            s_block_end()
             
         s_static("}")
 
@@ -101,42 +101,16 @@ def set_header_frame(headers):
             s_string(value, encoding="binary", fuzzable=False, max_length=0)
         """
     s_block_end()
-    s_size("all datas payload", endian=">", length=3, format="binary", fuzzable=False) # length(bytes number) of payload in frame
+
+    # Data
+    s_size("datas payload", endian=">", length=3, format="binary", fuzzable=False) # length(bytes number) of payload in frame
     s_bit_field(0, 8, fuzzable=False)  # type of frame
     s_bit_field(1, 8, fuzzable=False)  # flags of frame, End Stream
     s_bit_field(1, 32, endian=">", fuzzable=False, name="data stream id") # stream ID
     # payload (Header block fragment)
     # payload can't be too large
 
-    if s_block_start("all datas payload"):
-        s_group("datas group", ["fuck you konami"])
-        if s_block_start("datas payload", group="datas group"):
-            s_static("")
-        s_block_end()
+    if s_block_start("datas payload"):
+        s_static("fuck you konami")
     s_block_end()
-
-
-########################################################################################################################
-# Datas packet in HTTP/2 protocol
-########################################################################################################################
-s_initialize("HTTP/2 Datas")
-s_size("all datas payload", endian=">", length=3, format="binary", fuzzable=False) # length(bytes number) of payload in frame
-s_bit_field(0, 8, fuzzable=False)  # type of frame
-s_bit_field(1, 8, fuzzable=False)  # flags of frame, End Stream
-s_bit_field(1, 32, endian=">", fuzzable=False, name="data stream id") # stream ID
-# payload (Header block fragment)
-# payload can't be too large
-
-if s_block_start("all datas payload"):
-    s_group("datas group", ["fuck you konami"])
-    if s_block_start("datas payload", group="datas group"):
-        s_static("")
-    s_block_end()
-s_block_end()
-
-"""
-if s_block_start("datas payload"):
-    s_string("fuck you konami", encoding="binary", size=0, fuzzable=False)
-s_block_end()
-"""
 
